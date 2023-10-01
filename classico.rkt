3fad933b201965bf68bb6cf59d38bb55600b7f3a
Ana-CM <ana.mendes@ice.ufjf.br>
2023-10-01 18:15:33 -0300

    ordenação e novo nonterminal produzindo ε

--- classico.rkt
+++ classico.rkt
@@ -1,45 +1,64 @@
-
 #lang racket
 (require redex)
 
 ; Definição da gramática
 (define-language G
-    [nonterminal V]
-    [terminal number ε]
-    [flag *]
-    [rhs (seq ...)]
-    [seq (t ...) flag]
-    [t nonterminal terminal]
-    [production (nonterminal rhs)]
-    [V variable-not-otherwise-mentioned]
-    [grammar (production ...)])
+  [nonterminal V]
+  [terminal number ε]
+  [rhs (seq ...)]
+  [seq (t ...)]
+  [t nonterminal terminal]
+  [production (nonterminal rhs)]
+  [V variable-not-otherwise-mentioned]
+  [grammar (production ...)])
 
-
+; Redução
 (define g-->
-    (reduction-relation G
-          (--> [(nonterminal ((nonterminal t ...) seq ... ) ) production ...] 
-               [(nonterminal (seq ... (t ... nonterminal) ) ) production ...])
-          (--> [(nonterminal ((terminal t ...) seq ... ) ) production ...] 
-               [(nonterminal ( seq ...  (terminal t ...) ) ) production ...])
-          (--> [(nonterminal ((nonterminal_1 t ...) seq ... ) ) production ...] 
-               [(nonterminal ( seq ...  (nonterminal_1 t ...) ) ) production ...])  
-          (--> [(nonterminal (flag seq ... ) ) (nonterminal_1 (( t ...) seq_1 ... ) ) production ...]
-               [(nonterminal_1 (( t ...) seq_1 ... ) ) production ...  (nonterminal (flag seq ... ) )])
-    )
+  (reduction-relation G
+                      (--> [(nonterminal ((terminal t ...) ... (nonterminal t_1 ...) seq_2 ... )) production_1 ...] 
+                           [concat-grammar (production_1 ...) (left-recursion-elimination nonterminal ((terminal t ...) ... (nonterminal t_1 ...) seq_2 ... ))] "A -> Aα")
+                        
+                      (--> [(nonterminal ((terminal t ...) ... (nonterminal_1 t ...) ... ) production_1 ...)] 
+                           [production_1 ... (nonterminal ((terminal t ...) ... (nonterminal_1 t ...) ... ))])
+  ))
+
+; Função que elimina a recursão à esquerda direta
+(define-metafunction G
+  left-recursion-elimination : nonterminal rhs -> grammar
+
+  [(left-recursion-elimination nonterminal ((terminal t ...) ... (nonterminal t_1 ...) seq_2 ... )) 
+   ((nonterminal_new ((ε)))(nonterminal ((terminal t ...) ... (nonterminal t_1 ...) seq_2 ... )))
+   (where nonterminal_new ,(variable-not-in (term nonterminal) (term nonterminal) ))]
 )
 
-; Testes
+;-------------------- Funções auxiliares --------------------
 
-; S -> S 2 | S 4 | A
-; A -> A 3 | B
-; B -> B | 2
-(module+ main
-(traces g--> ( term (
-                   (S ((1 2) (S 2) *))
-                   (B ((B 3) (B 4) *))
-                   (C ((ε) *))
-                   (D ((1 3) *))
-                ))))
+; Função que unifica duas gramaticas
+(define-metafunction G
+  concat-grammar : grammar grammar -> grammar
 
-; Cvs sobre o "*" no final da sequência
-; cvs sobre a criação de um novo n terminal
+  [(concat-grammar (production_1 ... ) (production_2 ... )) 
+   (production_1 ... production_2 ... )]
+)
+
+; Função que ordena a gramática
+(define (order-rhs productions)
+  (map
+   (lambda (p)
+     (define rhs (car(cdr p)))
+            
+     (let ((terminal (filter (lambda (x) (or (number? (car x)) (equal? (car x) 'ε))) rhs))  
+           (nonterminal (filter (lambda (x) (not (or (number? (car x)) (equal? (car x) 'ε)))) rhs))) 
+       (cons (car p) (list(append terminal nonterminal))))
+     )
+  productions))
+
+;-------------------- Testes --------------------
+(define ordered-productions
+  (order-rhs '(
+               (S ((S 2) (4) (2)))
+               (A ((A 3) (B 1) (A B)))
+               (B ((B) (2)))
+               )))
+
+(traces g--> ordered-productions)
