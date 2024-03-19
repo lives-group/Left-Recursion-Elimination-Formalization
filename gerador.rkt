cc0b780a66917c30772aaef5134cd04be0ccc147
Ana-CM <ana.mendes@ice.ufjf.br>
2024-03-18 22:41:59 -0300

    melhorias na gramatica para gerar palavras

--- gerador.rkt
+++ gerador.rkt
@@ -17,15 +17,15 @@
 (define (generate-terminals size)
   (let loop ((n 1) (acc '()))
     (if (> n size)
-        acc
-        (loop (+ n 1) (cons n acc)))))
+      acc
+      (loop (+ n 1) (cons n acc)))))
 
 ; Gera os não-terminais
 (define (generate-nonterminals size)
-    (let loop ((n 1) (acc '()))
-        (if (> n size)
-            acc
-             (loop (+ n 1) (cons (list-ref '(S A B C D E F G H I J K L M N O P Q R T U V W X Y Z) (- n 1)) acc)))))
+  (let loop ((n 1) (acc '()))
+    (if (> n size)
+      acc
+        (loop (+ n 1) (cons (list-ref '(S A B C D E F G H I J K L M N O P Q R T U V W X Y Z) (- n 1)) acc)))))
 
 ; Sorteia o tamanho do rhs
 (define (get-rhs-size)
@@ -38,20 +38,20 @@
 ; Sorteia um número entre 1 e 3 
 ; (1 = recursão direta, 2 = chance de recursão indireta, 3 = sorteio de terminal ou não-terminal)
 (define (get-seq-type)
-    (+ 1 (random 3)))
+  (+ 1 (random 3)))
 
 ; Sorteia um terminal ou não-terminal
 (define (get-term terminals nonterminals)
     (if (< (random 2) 1)
-        (list-ref terminals (random (length terminals)))
-        (list-ref nonterminals (random (length nonterminals)))))
+      (list-ref terminals (random (length terminals)))
+      (list-ref nonterminals (random (length nonterminals)))))
 
 ; Sorteia um nonteminal que esteja depois do não-terminal passado como parâmetro na lista de não-terminais
 (define (sort-nonterminal nonterminal nonterminals)
   (define index (position nonterminal nonterminals 0))
   (if (zero? index)
-      (random-elt nonterminals)
-      (random-elt (drop nonterminals index))))
+    (random-elt nonterminals)
+    (random-elt (drop nonterminals index))))
 
 ; Sorteia um item da lista
 (define (random-elt lst)
@@ -60,51 +60,62 @@
 ; Retorna o índice do item na lista
 (define (position item list count)
    (if (equal? item (car list))
-       count
-       (position item (cdr list) (+ count 1))))
+      count
+      (position item (cdr list) (+ count 1))))
 
 ; Gera uma gramática
 (define (generate-grammar terminals nonterminals)
-    (let loop ((n 0) (acc '()))
-        (if (= n (length nonterminals))
-            acc
-            (loop (+ n 1) (cons (generate-production (list-ref nonterminals n) terminals nonterminals) acc)))))       
+  (let loop ((n 0) (acc '()))
+    (if (= n (length nonterminals))
+      acc
+      (loop (+ n 1) (cons (generate-production (list-ref nonterminals n) terminals nonterminals) acc)))))       
 
 ; Gera uma produção
 (define (generate-production nonterminal terminals nonterminals)
-    (define rhs-size (get-rhs-size))
-    (list nonterminal 
-    (let loop ((n 0) (acc '()))
+  (define rhs-size
+    (if (eq? 'S nonterminal)
+        max-rhs
+        (get-rhs-size)))
+  (list nonterminal 
+    (append
+      (let loop ((n 0) (acc '()))
         (if (= n rhs-size)
-            acc
-            (loop (+ n 1) 
-              (cons (generate-sequence nonterminal terminals nonterminals) acc))))))
+          acc
+          (loop (+ n 1) 
+            (cons (generate-sequence nonterminal terminals nonterminals) acc))))
+    (list (generate-sequence-terminal terminals)))))
+
+; Gera uma sequência de terminais
+(define (generate-sequence-terminal terminals)
+  (define seq-size (get-seq-size))
+  (let loop ((n 0) (acc '()))
+    (if (= n seq-size)
+      acc
+      (loop (+ n 1) 
+        (cons (get-term terminals terminals) acc)))))
 
 ; Gera uma sequência
 (define (generate-sequence nonterminal terminals nonterminals)
-    (define seq-size (get-seq-size))
-    (let loop ((n 0) (acc '()))
-        (if (= n seq-size)
-             (append acc (list (get-term terminals terminals)))
-            (loop (+ n 1) 
-              (cons (generate-seq-item nonterminal terminals nonterminals n seq-size) acc)))))
+  (define seq-size
+    (if (eq? 'S nonterminal)
+      max-seq
+      (get-seq-size)))
+  (let loop ((n 0) (acc '()))
+    (if (= n seq-size)
+      (append acc (list (get-term terminals terminals)))
+      (loop (+ n 1) 
+        (cons (generate-seq-item nonterminal terminals nonterminals n seq-size) acc)))))
 
 ; Gera um item da sequência
 (define (generate-seq-item nonterminal terminals nonterminals index seq-size)
   (define seq-type (get-seq-type))
-  
-  (if (= seq-size 1)
-    (if (zero? index)
-        (list-ref terminals (random (length terminals)))
-        (if (< (random 2) 1)
-          (list-ref terminals (random (length terminals)))
-          (sort-nonterminal nonterminal nonterminals)))
-
-    (if (zero? index)
-        (cond ((= seq-type 1) nonterminal)
-          ((= seq-type 2) (sort-nonterminal nonterminal nonterminals))
-          ((= seq-type 3) (get-term terminals nonterminals)))
-        (get-term terminals nonterminals))))
+  (if (eq? 'S nonterminal)
+    (get-term nonterminals nonterminals)
+    (if (= (+ index 1) seq-size)
+      (cond ((= seq-type 1) nonterminal)
+        ((= seq-type 2) (sort-nonterminal nonterminal nonterminals))
+        ((= seq-type 3) (get-term terminals nonterminals)))
+      (get-term terminals nonterminals))))
 
 ; --- Debug ---
 
@@ -118,10 +129,10 @@
         (loop (+ n 1)))
       (displayln ")"))))
 
-;(define terminals (generate-terminals 4))
-;(define nonterminals (generate-nonterminals 3))
-;(define teste (generate-grammar terminals nonterminals))
-;(print-grammar teste)
+;;; (define terminals (generate-terminals 8))
+;;; (define nonterminals (generate-nonterminals 5))
+;;; (define teste (generate-grammar terminals nonterminals))
+;;; (print-grammar teste)
 
 ; --- Chamada do algoritmo clássico ---
 #;(define ordered-productions
