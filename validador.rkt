733e3ab4337b251f0bd79da05d7b933559d09ff0
Ana-CM <ana.mendes@ice.ufjf.br>
2024-05-03 08:40:02 -0300

    finalizando testes

--- validador.rkt
+++ validador.rkt
@@ -16,7 +16,7 @@
 ; Quatidade de palavras 
 (define num-words 10)
 
-#;(module+ test
+(module+ test
   ; Gera os terminais
   (define gen:terminals
     (gen:let ([num-terminals (gen:integer-in min-terminals max-terminals)])
@@ -50,23 +50,22 @@
       (define g2 (car (apply-reduction-relation* i--> g1)))
 
       ; Verifica se as recursões à esquerda foram removidas
-      ;(check-equal? (has-left-recursion? g2) #f)
+      (check-equal? (has-left-recursion? g2) #f)
 
       ; Verifica se termos aceitos por g1 são aceitos por g2
       (check-equal? (accepts? g1 g2) #t)
       
       ; Verifica se termos aceitos por g2 são aceitos por g1
-      ;(check-equal? (accepts? g2 g1) #t)
+      (check-equal? (accepts? g2 g1) #t)
       
       ; Verifica se os termos NÃO aceitos por g1 são aceitos por g2
-      ;(check-equal? (not-accepts? g1 g2) #t)
+      (check-equal? (not-accepts? g1 g2) #t)
+      
       ; Verifica se os termos NÃO aceitos por g2 são aceitos por g1
-      ;(check-equal? (not-accepts? g2 g1) #t)   
+      (check-equal? (not-accepts? g2 g1) #t)   
       )))
 
-
 ;---- Funções auxiliares ----
-
 ; Verifica se na gramática existe alguma produção recursiva à esquerda
 (define (has-left-recursion? grammar)
   (define productions (car (cdr grammar)))
@@ -78,7 +77,6 @@
       (define rhs (car(cdr prd)))
       (or  (is-left-recursive-d head rhs) (is-left-recursive-i head rhs orders)))
     productions))
-
 
 ; Verifica se uma produção é recursiva à esquerda  de forma direta
 (define (is-left-recursive-d term rhs)
@@ -130,70 +128,24 @@
 
     ; Gera os termos de gf
     (define terms (sample (gen:word-from-grammar g-first) num-words))
-  
     (ormap
       (lambda (term)
-        (displayln term)
-
-        (if (not (equal? term '∅)) 
-          (if (accepts-term? (car term) g-second) #t #t)
+        (if (and (not (equal? term '∅)) (not (equal? term #f)))
+          (if (in-grammar? g-second term) #t #f)
           #t)) 
       terms))
-
-; Gera os termos de uma gramática
-(define (generate-terms grammar)
-    ; para fim de teste, retorna os numeros de 0 a 100 que sejam par
-    (range 0 100 2))
 
 ; Verifica se a gramática gs não aceita termos que não são aceitos por gf   
 (define (not-accepts? gf gs)
     (define g-first  (format-input(car (cdr gf))))
     (define g-second (format-input(car (cdr gs))))
-    (print-grammar g-first)
-    
-    (define not-terms (generate-not-terms g-first))
 
+    ; Gera os termos de gf
+    (define terms (sample (gen:word-from-grammar g-first) num-words))
     (ormap
-        (lambda (term)
-            ; Verifica se o termo é aceito por gs
-            (if (accepts-term? term g-second) #f #t))
-        not-terms))
-  
-; Gera os termos que não são aceitos por uma gramática
-(define (generate-not-terms grammar)
-    ; para fim de teste, retorna os numeros de 0 a 100 que sejam ímpar
-    (range 1 100 2))
+      (lambda (term)
+        (if (and (not (equal? term '∅)) (not (equal? term #f)))
+           (if (in-grammar? g-second (cons 0 (append term '(0)))) #t #f)
+          #t)) 
+      terms))
 
-; Verifica se um termo é aceito por uma gramática
-(define (accepts-term? term grammar)
-    ; para fim de teste, se for um número par, retorna true, senão, false
-    (if (even? term) #t #f))
-
-
-; --- Debug ---
-
-(define input
-  '(
-        (S ((4 A 6 7) (A 5 3)))
-        (A ((C S 2) (1 2)))
-        (C ((8 6 2) (2)))
-    ))
-
-;(define teste (order-rhs input))
-;(define order (car teste))
-
-;(display (before-list order 'C))
-;(display (has-left-recursion? teste))
-
-; Imprime a gramática
-(define (print-grammar grammar)
-  (displayln "(")
-  (let loop ((n 0))
-    (if (< n (length grammar))
-      (begin
-        (displayln (list-ref grammar n))
-        (loop (+ n 1)))
-      (displayln ")"))))
-      
-;gen:word-from-grammar
-;gen:distinct-word-from-grammar
