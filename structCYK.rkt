4e773505d08ad24c3e66608797e30949bdde0907
Ana-CM <ana.mendes@ice.ufjf.br>
2024-06-19 22:27:31 -0300

    produções individuais

--- structCYK.rkt
+++ structCYK.rkt
@@ -7,46 +7,38 @@
     "struct.rkt")
 
 (define entradaTeste (format-input '(
-               (S ((B 2 3)))
+               (S ((B 2 3) ()))
                (B ((B) (S 2)))
                (C ((A) (3) (4 5) (1) (D 5)))
                )))
 
-(struct prod (nt rhs) #:transparent)
-
-(define (make-pars xs) 
-    (match xs 
-        ['() '()]
-        [(cons x xs) (cons (make-splits x) (make-pars xs))]))
-
-(define (make-splits x)
-    (match x
-        [(cons nt rhs) (make-splits2 nt rhs)]))
-
-(define (make-splits2 nt rhs)
-    (match rhs
-        ['() '()]
-        [(cons x xs) (cons (prod nt x) (make-splits2 nt xs))]))
 
 (define (format-input-cyk input)
-    (define (format-prd prd)
-        (match prd
-        [(Production (NT nt) alt)
-          (cons nt (coalese-alt (rewrite-rhs alt)))]))
+  (define (format-prd prd)
+    (match prd
+      [(Production (NT nt) alt)
+       (let ((formatted-rhs (coalese-alt (rewrite-rhs alt))))
+         (map (lambda (rhs) (cons nt (list rhs))) formatted-rhs))]))
 
-    (define (rewrite-rhs rhs)
-      (match rhs 
-        [(Alt e d) (Alt (rewrite-rhs e) (rewrite-rhs d))]
-        [(Seq (T t) d) (cons t (rewrite-rhs d))]
-        [(Seq (NT nt) d) (cons nt (rewrite-rhs d))]
-        [(NT nt) (list nt)]
-        [(T t) (list t)]))
-    
-    (define (coalese-alt rhs)
-      (match rhs 
-        [(Alt e d) (list* (car (coalese-alt e)) (coalese-alt d))]
-        [x (list x)]))
-    
-    (define formatted-input (map format-prd input)) formatted-input)
+  (define (rewrite-rhs rhs)
+    (match rhs 
+      [(Alt e d) (Alt (rewrite-rhs e) (rewrite-rhs d))]
+      [(Seq (T t) d) (cons t (rewrite-rhs d))]
+      [(Seq (NT nt) d) (cons nt (rewrite-rhs d))]
+      [(NT nt) (list nt)] 
+      [(T t) (list t)]
+      [#t (list )] ))
 
-(displayln (make-pars(format-input-cyk entradaTeste)))
+  (define (coalese-alt rhs)
+    (match rhs 
+      [(Alt e d) (append (coalese-alt e) (coalese-alt d))]
+      [x (list x)]))
+  
+  (define (flatten list-of-lists)
+    (apply append list-of-lists))
+
+  (define formatted-input (flatten (map format-prd input)))
+  formatted-input)
+
+
+(displayln (format-input-cyk entradaTeste))
