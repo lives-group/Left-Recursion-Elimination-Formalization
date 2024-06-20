6a3860fd6bb65460026085c6d257853342b1b2ec
Ana-CM <ana.mendes@ice.ufjf.br>
2024-06-20 14:19:24 -0300

    finalizando formatação cyk

--- structCYK.rkt
+++ structCYK.rkt
@@ -7,23 +7,26 @@
     "struct.rkt")
 
 (define entradaTeste (format-input '(
-               (S ((B 9 3) (5 3)))
+               (S ((B 9 3) (5 3 7)))
                (B ((B) (S 2 1 2)))
                (C ((A) (3 4) (4 5 5) (1) (D 5)))
                )))
 
-
 (define (format-input-cyk input)
   (define counter 1)
-  
+  (define terminals '())
+
   (define (generate-new-nt nt)
     (let ((new-nt (string->symbol (string-append "AUX" (number->string counter)))))
       (set! counter (+ counter 1))
       new-nt))
-  
-   (define (num->symbol n)
-    (string->symbol (string (integer->char (+ n 97)))))
-  
+
+  (define (num->symbol n)
+    (let ((sym (string->symbol (string-append "T" (number->string n)))))
+      (unless (assoc sym terminals)
+        (set! terminals (cons (cons sym (string->symbol (string (integer->char (+ n 97))))) terminals)))
+      sym))
+
   (define (format-prd prd)
     (match prd
       [(Production (NT nt) alt)
@@ -43,10 +46,10 @@
     (match rhs 
       [(Alt e d) (append (coalese-alt e) (coalese-alt d))]
       [x (list x)]))
-  
+
   (define (process-rhs nt rhs)
     (if (<= (length rhs) 2)
-        (list (cons nt (list rhs)))
+        (list (cons nt (list rhs)))  ;; Ensure RHS is always wrapped in a list
         (let ((new-nt (generate-new-nt nt)))
           (append (list (list nt (list (car rhs) new-nt)))
                   (process-rhs new-nt (cdr rhs))))))
@@ -55,6 +58,8 @@
     (apply append list-of-lists))
 
   (define formatted-input (flatten (map format-prd input)))
-  formatted-input)
+  (define terminal-productions (map (lambda (t) (list (car t) (list (cdr t))))
+                                    terminals))
+  (append formatted-input terminal-productions))
 
 (displayln (format-input-cyk entradaTeste))
